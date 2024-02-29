81d18b7b772e764fb8b8dc7084caffbb06dbdc1a
CristianBustamante <93169424+cdbustamante@users.noreply.github.com>
2024-02-29 17:53:30 -0500

    se agregan cambios
    se agregan cambios realizados por equipo

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Card, CardContent, Typography } from "@mui/material";
 
-const RepoCard = ({ repoName, repoSize }) => {
+const RepoCard = ({ repoName, repoSize, stars, created, language }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
@@ -15,9 +15,20 @@ const RepoCard = ({ repoName, repoSize }) => {
           {repoName}
         </Typography>
         {isHovered && (
-          <Typography variant="body2" align="center" className="repo-size">
-            Tamaño: {repoSize}
-          </Typography>
+          <>
+            <Typography variant="body2" align="center" className="repo-size">
+              Tamaño: {repoSize}
+            </Typography>
+            <Typography variant="body2" align="center" className="repo-created">
+              Fecha de Creación: {created}
+            </Typography>
+            <Typography variant="body2" align="center" className="repo-stars">
+              Estrellas: {stars}
+            </Typography>
+            <Typography variant="body2" align="center" className="repo-language">
+              Idioma: {language}
+            </Typography>
+          </>
         )}
       </CardContent>
     </Card>
--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
-import RepoCard from "./RepoCard"; // Importa el componente RepoCard
+import RepoCard from "./RepoCard";
+import { Pagination, TextField, MenuItem, Select, InputLabel, FormControl } from "@mui/material";
 
 const RepoList = ({ username }) => {
   const [repos, setRepos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [filter, setFilter] = useState("size"); // Predeterminado: ordenar por tamaño
+  const reposPerPage = 7;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,9 +16,11 @@ const RepoList = ({ username }) => {
         const response = await axios.get(
           `https://api.github.com/users/${username}/repos`
         );
-        const sortedRepos = response.data.sort((a, b) => b.size - a.size);
-        const topRepos = sortedRepos.slice(0, 5);
-        setRepos(topRepos);
+        const reposWithDate = response.data.map((repo) => ({
+          ...repo,
+          created_at: new Date(repo.created_at),
+        }));
+        setRepos(reposWithDate);
       } catch (error) {
         console.error("Error fetching repos:", error);
       }
@@ -23,15 +29,75 @@ const RepoList = ({ username }) => {
     fetchData();
   }, [username]);
 
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const compareFunction = (a, b) => {
+    if (filter === "size") {
+      return b.size - a.size;
+    } else if (filter === "stars") {
+      return b.stargazers_count - a.stargazers_count;
+    } else if (filter === "created") {
+      return b.created_at - a.created_at;
+    } else if (filter === "language") {
+      return a.language.localeCompare(b.language);
+    }
+    return 0;
+  };
+
+  const formatDate = (date) => {
+    const options = { day: 'numeric', month: 'numeric', year: 'numeric' };
+    return new Intl.DateTimeFormat('es-ES', options).format(date);
+  };
+
+  const paginatedRepos = repos.slice((page - 1) * reposPerPage, page * reposPerPage);
+
   return (
     <div>
-      <h2>Top 5 repositorios con más participación de {username}</h2>
+      <h2>Repositorios de {username}</h2>
+      {/* Selector de filtro */}
+      <FormControl fullWidth>
+        <InputLabel id="filter-label">Filtrar por:</InputLabel>
+        <Select
+          labelId="filter-label"
+          id="filter"
+          value={filter}
+          onChange={handleFilterChange}
+          label="Filtrar por:"
+        >
+          <MenuItem value="size">Tamaño</MenuItem>
+          <MenuItem value="stars">Estrellas</MenuItem>
+          <MenuItem value="created">Fecha de Creación</MenuItem>
+          <MenuItem value="language">Idioma</MenuItem>
+        </Select>
+      </FormControl>
       <ul>
-        {repos.map((repo) => (
-          // Pasa el nombre y el tamaño del repositorio como propiedades
-          <RepoCard key={repo.id} repoName={repo.name} repoSize={repo.size} />
+        {paginatedRepos.sort(compareFunction).map((repo) => (
+          <RepoCard
+            key={repo.id}
+            repoName={repo.name}
+            repoSize={repo.size}
+            stars={repo.stargazers_count}
+            created={formatDate(repo.created_at)}
+            language={repo.language}
+          />
         ))}
       </ul>
+      <div className="pag-container">
+        <Pagination
+          count={Math.ceil(repos.length / reposPerPage)}
+          page={page}
+          onChange={handlePageChange}
+          variant="outlined"
+          shape="rounded"
+          size="large"
+        />
+      </div>
     </div>
   );
 };
